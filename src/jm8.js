b3760775185e3366894246833145ce859f38f6d6
Sebastian Börgers <s.boergers@live.de>
2021-03-15 00:27:04 +0100

    Bit of refactoring and error correction

--- src/jm8.js
+++ src/jm8.js
@@ -2,30 +2,25 @@ const jm8 = {
   /*
     Cookie Manipulation
     Not sorted under doc for several reasons (e.g. frequent use => quicker access)
-    */
+  */
   cookie: {
     set: (cookieName, cookieValue, expirationDays) => {
       var maxAge =
         expirationDays === 0 ? "" : "Max-Age=" + (expirationDays * 24 * 60 * 60).toString() + ";";
-
-      if (HOME_STRINGS.includes(SITE_HOSTNAME) || SITE_HOSTNAME === "127.0.0.1") {
-        document.cookie = cookieName + "=" + cookieValue + ";" + maxAge + "SameSite=Lax;Path=/";
-      } else {
-        document.cookie = cookieName + "=" + cookieValue + ";" + maxAge + "SameSite=None;Secure;Path=/";
-      }
+      document.cookie = cookieName + "=" + cookieValue + ";" + maxAge + "SameSite=None;Secure;Path=/";
       return cookieValue;
     },
     get: (cookieName) => {
       var name = cookieName + "=";
       var decodedCookie = decodeURIComponent(document.cookie);
-      var ca = decodedCookie.split(";");
-      for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == " ") {
-          c = c.substring(1);
+      var cookieArray = decodedCookie.split(";");
+      for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) == " ") {
+          cookie = cookie.substring(1);
         }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
+        if (cookie.indexOf(name) == 0) {
+          return cookie.substring(name.length, cookie.length);
         }
       }
       return undefined;
@@ -33,7 +28,7 @@ const jm8 = {
   },
   /*
     Shortcuts for Document Functionality
-    */
+  */
   doc: {
     queryString: {
       get: (key) => {
@@ -45,18 +40,19 @@ const jm8 = {
         }
       },
     },
-  },
-  getLanguageFromHtml: () => {
-    let lang = document.documentElement.lang;
-    if (lang != undefined && lang.length != 0) {
-      return lang.slice(0, 2).toLowerCase();
-    } else {
-      return undefined;
-    }
+    HtmlLanguageTag: {
+      get: () => {
+        let lang = document.documentElement.lang;
+        if (lang != undefined && lang.length != 0) {
+          return lang.slice(0, 2).toLowerCase();
+        } else {
+          return undefined;
+        }
+      },
   },
   /*
     Description Goes Here
-    */
+  */
   convert: {
     linkFormatter: () => {
       let isHttps = link.indexOf("https://");
@@ -79,14 +75,13 @@ const jm8 = {
   },
   /*
     Description Goes Here
-    */
+  */
   window: {
-    scrollToId: (id) => {
+    scrollToId: (id, offset = 0) => {
       let elemScrollY = document.getElementById(id).getBoundingClientRect().top;
       let bodyY = document.body.getBoundingClientRect().top;
-      let headerHeight = document.getElementById("navMenu").getBoundingClientRect().height || 0;
       window.scrollTo({
-        top: elemScrollY - bodyY - headerHeight,
+        top: elemScrollY - bodyY - offset,
         behavior: "smooth",
       });
     },
@@ -104,5 +99,6 @@ const jm8 = {
         return [sx, sy];
       }
     },
+    //getElementScroll(){}
   },
 };
